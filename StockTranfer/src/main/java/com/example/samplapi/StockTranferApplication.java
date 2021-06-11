10420769f0fa75b45305ec30991739ec98a4ee3a
S27ultron <73684483+S27ultron@users.noreply.github.com>
2021-06-11 15:53:51 +0530

    New Project Created

---
+++ StockTranfer/src/main/java/com/example/samplapi/StockTranferApplication.java
@@ -0,0 +1,13 @@
+package com.example.samplapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockTranferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockTranferApplication.class, args);
+	}
+
+}
